4d146d17f7e2810f63b8f0033177d37fcd18dfec
github username <github email address>
2017-06-27 22:42:10 -0400

    added redux

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './index.css';
+import { createStore } from 'redux'
+
+var defaultState = {
+  msgcount: 0,
+  messages: []
+}
+
+const store = createStore(reducer)
+const reducer = (state = defaultState,action)=>{
+  //takes in state and action and reduces
+  console.log(action)
+}
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
